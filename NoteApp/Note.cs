dd0c14aa919e3070aceb32f2b36b9d1ec1591bee
1Zarkos1 <zarkos777@gmail.com>
2023-09-07 10:59:08 +0700

    Add project files.

---
+++ NoteApp/Note.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoteApp
+{
+    class Note
+    {
+        private string _text;
+        private bool _completed;
+        public readonly DateTime DateCreated;
+        public string Text { get => _text; set => _text = value; }
+        public bool Completed { get => _completed; set => _completed = value; }
+
+        public Note(string text, bool completed = false) 
+        {
+            Text = text;
+            DateCreated = DateTime.Now;
+            Completed = completed;
+        }
+
+        public override string ToString()
+        {
+            return $"{Text}; on: {DateCreated}";
+        }
+    }
+}
---
+++ NoteApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Windows;
+
+namespace NoteApp
+{
+    class MainApp
+    {
+        public static void Main()
+        {
+            List<Note> notes = new List<Note>();    
+
+            while (true)
+            {
+                int index = 1;
+                Console.WriteLine("\nType your note below:");
+                var noteText = Console.ReadLine();
+                if (noteText != null)
+                {
+                    notes.Add(new Note(noteText));
+                    foreach (var note in notes)
+                    {
+                        Console.WriteLine($"{index}. {note}");
+                        ++index;
+                    }
+                } else { 
+                    break; 
+                }
+            }
+        }
+    }
+}
